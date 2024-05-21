6696566528feedcef3def9794a040b57dde822f0
Kalinka5 <dania003d@gmail.com>
2024-05-21 19:16:13 +0300

    Add cards to Payment page

--- frontend/src/components/Payment.js
+++ frontend/src/components/Payment.js
@@ -5,67 +5,104 @@ class Payment extends React.Component {
   render() {
     return (
       <div className="payment">
-        <div class="tips">
-          Payment card number: (4) VISA, (51-55) MasterCard, (36-38-39)
-          DinersClub, (34-37) American Express, (65) Discover, (5019) dankort
-        </div>
-
-        <div class="container">
-          <div class="col1">
-            <div class="card">
-              <div class="front">
-                <div class="type">
-                  <img class="bankid" alt="Payment card" />
+        <div className="block row1">
+          <div className="container-mastercard">
+            <div className="front-card">
+              <div className="column1">
+                <h3 id="main-title">
+                  monobank | <span>Universal Bank</span>
+                </h3>
+                <div id="chip"></div>
+                <div className="card-info">
+                  <p id="no">5375 4141 2253 7789</p>
+                  <div className="grid-date">
+                    <p id="name">Daniil Kalinevych</p>
+                    <p id="exp-date">07/26</p>
+                  </div>
                 </div>
-                <span class="chip"></span>
-                <span class="card_number">
-                  &#x25CF;&#x25CF;&#x25CF;&#x25CF;
-                  &#x25CF;&#x25CF;&#x25CF;&#x25CF;
-                  &#x25CF;&#x25CF;&#x25CF;&#x25CF;
-                  &#x25CF;&#x25CF;&#x25CF;&#x25CF;{" "}
-                </span>
-                <div class="date">
-                  <span class="date_value">MM / YYYY</span>
-                </div>
-                <span class="fullname">FULL NAME</span>
               </div>
-              <div class="back">
-                <div class="magnetic"></div>
-                <div class="bar"></div>
-                <span class="seccode">&#x25CF;&#x25CF;&#x25CF;</span>
-                <span class="chip"></span>
-                <span class="disclaimer">
-                  This card is property of Random Bank of Random corporation.{" "}
-                  <br /> If found please return to Random Bank of Random
-                  corporation - 21968 Paris, Verdi Street, 34{" "}
-                </span>
+              <div className="column2">
+                <i id="globe" className="fa fa-globe"></i>
+                <div id="mastercard"></div>
               </div>
             </div>
           </div>
-          <div class="col2">
-            <label>Card Number</label>
+        </div>
+        <div className="block row1">
+          <div className="description">
+            <h2>Ви також можете зробити свій внесок через банку Фонду</h2>
+            <a
+              href="https://send.monobank.ua/jar/QsATQ1NQ4"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Монобанка
+            </a>
+          </div>
+        </div>
+        <div className="block">
+          <div className="container-visa">
+            <div className="front">
+              <img
+                src="https://i.ibb.co/PYss3yv/map.png"
+                className="map-img"
+                alt="Map"
+              />
+              <div className="row">
+                <img
+                  src="https://i.ibb.co/G9pDnYJ/chip.png"
+                  width="60px"
+                  alt="Chip"
+                />
+                <img
+                  src="https://i.ibb.co/WHZ3nRJ/visa.png"
+                  width="60px"
+                  alt="Visa"
+                />
+              </div>
+              <div className="row card-no">
+                <p>5244</p>
+                <p>2150</p>
+                <p>8252</p>
+                <p>6420</p>
+              </div>
+              <div className="row card-holder">
+                <p>CARD HOLDER</p>
+                <p>VALID TILL</p>
+              </div>
+              <div className="row name">
+                <p>Daniil Kalinevych</p>
+                <p>10 / 25</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="block">
+          <div className="description">
+            <label>Одержувач</label>
             <input
-              class="number"
+              className="number"
               type="text"
-              ng-model="ncard"
-              maxlength="19"
-              onkeypress="return event.charCode >= 48 && event.charCode <= 57"
+              defaultValue="Данііл Каліневич"
             />
-            <label>Cardholder name</label>
-            <input class="inputname" type="text" placeholder="" />
-            <label>Expiry date</label>
-            <input class="expire" type="text" placeholder="MM / YYYY" />
-            <label>Security Number</label>
+            <label>IBAN</label>
             <input
-              class="ccv"
+              className="number"
               type="text"
-              placeholder="CVC"
-              maxlength="3"
-              onkeypress="return event.charCode >= 48 && event.charCode <= 57"
+              defaultValue="UA843052990000026200681993072"
             />
-            <button class="buy">
-              <i class="material-icons">lock</i> Pay --.-- €
-            </button>
+            <label>РНОКПП</label>
+            <input
+              className="inputname"
+              type="text"
+              defaultValue="2975800618"
+            />
+            <label>Призначення платежу</label>
+            <input
+              className="expire"
+              type="text"
+              defaultValue="Благодійний безповоротний внесок"
+            />
           </div>
         </div>
       </div>
