05880cd4b0a05747e31f42711fff0b28c4368895
Kalinka5 <dania003d@gmail.com>
2024-05-30 22:06:31 +0300

    Add Choreo host to ALLOWED_HOSTS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,7 +33,9 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'backend-613209188-choreo-internal.dp-development-kalinafond-42344-3707085086'
+]
 
 
 # Application definition
