05ee09897ef1e58ab095816c25801ce1ddfae37d
Kalinka5 <dania003d@gmail.com>
2024-07-27 16:13:42 +0300

    Updated Login page in landscape orientation

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -6,15 +6,27 @@ import { IoMdEye, IoMdEyeOff } from "react-icons/io";
 import api from "../api";
 import { ACCESS_TOKEN, REFRESH_TOKEN, PROFILE_PAGE } from "../constants";
 
+import useOrientation from "../useOrientation";
+
+import loginImg1 from "../img/login-img1.jpg";
+import loginImg2 from "../img/login-img2.jpg";
+import loginImg3 from "../img/login-img3.jpg";
+import loginImg4 from "../img/login-img4.jpg";
+
 import "../styles/login.css";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [loading, setLoading] = useState(false);
+
   const [passVisible, setPassVisible] = useState(false);
   const [passType, setPassType] = useState("password");
+
+  const [loading, setLoading] = useState(false);
+
   const navigate = useNavigate();
+
+  const orientation = useOrientation();
 
   const handleSubmit = async (e) => {
     setLoading(true);
@@ -46,63 +58,126 @@ function Login() {
   return (
     <div className="login">
       <div className="login-register">
-        <div className="log-reg-background">
-          <div className="shape log-shape1"></div>
-          <div className="shape log-shape2"></div>
-        </div>
-        <form className="log-reg-form log-p" onSubmit={handleSubmit}>
-          <h3 className="log-h3">Login Here</h3>
+        {orientation.isPortrait ? (
+          <div className="portrait">
+            <div className="log-reg-background">
+              <div className="shape log-shape1"></div>
+              <div className="shape log-shape2"></div>
+            </div>
+            <form className="log-reg-form log-p" onSubmit={handleSubmit}>
+              <h3 className="log-h3">Login Here</h3>
 
-          <label className="email-label" htmlFor="email">
-            Email
-          </label>
-          <div className="log-input-box">
-            <input
-              type="text"
-              value={email}
-              placeholder="Your Email"
-              id="email"
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
+              <label className="email-label" htmlFor="email">
+                Email
+              </label>
+              <div className="log-input-box">
+                <input
+                  type="text"
+                  value={email}
+                  placeholder="Your Email"
+                  id="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+
+              <label htmlFor="password">Password</label>
+              <div className="log-input-box">
+                <input
+                  type={passType}
+                  value={password}
+                  placeholder="Your Password"
+                  id="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+                <span className="password-toggle-icon" onClick={clickIcon}>
+                  {passVisible ? (
+                    <i>
+                      <IoMdEyeOff />
+                    </i>
+                  ) : (
+                    <i>
+                      <IoMdEye />
+                    </i>
+                  )}
+                </span>
+              </div>
+
+              <div className="btn-container log-button">
+                <button type="submit">
+                  Log In
+                  {loading && <div className="loader"></div>}
+                </button>
+              </div>
+
+              <p className="log-p">
+                You don't have an account?
+                <br />
+                <a href="/register">Register now</a>
+              </p>
+            </form>
           </div>
+        ) : (
+          <div className="landscape">
+            <div className="image column">
+              <img src={loginImg4} alt="Login" />
+            </div>
+            <form className="log-reg-form log-p column" onSubmit={handleSubmit}>
+              <h3 className="log-h3">Login Here</h3>
 
-          <label htmlFor="password">Password</label>
-          <div className="log-input-box">
-            <input
-              type={passType}
-              value={password}
-              placeholder="Your Password"
-              id="password"
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-            <span className="password-toggle-icon" onClick={clickIcon}>
-              {passVisible ? (
-                <i>
-                  <IoMdEyeOff />
-                </i>
-              ) : (
-                <i>
-                  <IoMdEye />
-                </i>
-              )}
-            </span>
+              <label className="email-label" htmlFor="email">
+                Email
+              </label>
+              <div className="log-input-box">
+                <input
+                  type="text"
+                  value={email}
+                  placeholder="Your Email"
+                  id="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+
+              <label htmlFor="password">Password</label>
+              <div className="log-input-box">
+                <input
+                  type={passType}
+                  value={password}
+                  placeholder="Your Password"
+                  id="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+                <span className="password-toggle-icon" onClick={clickIcon}>
+                  {passVisible ? (
+                    <i>
+                      <IoMdEyeOff />
+                    </i>
+                  ) : (
+                    <i>
+                      <IoMdEye />
+                    </i>
+                  )}
+                </span>
+              </div>
+
+              <div className="btn-container log-button">
+                <button type="submit">
+                  Log In
+                  {loading && <div className="loader"></div>}
+                </button>
+              </div>
+
+              <p className="log-p">
+                You don't have an account?
+                <br />
+                <a href="/register">Register now</a>
+              </p>
+            </form>
           </div>
-
-          <div className="btn-container log-button">
-            <button type="submit">
-              Log In
-              {loading && <div className="loader"></div>}
-            </button>
-          </div>
-
-          <p className="log-p">
-            You don't have an account?
-            <br />
-            <a href="/register">Register now</a>
-          </p>
-        </form>
+        )}
       </div>
     </div>
   );
---
+++ frontend/src/useOrientation.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import { Dimensions } from "react-native";
+
+const useOrientation = () => {
+  const [screenInfo, setScreenInfo] = useState(Dimensions.get("screen"));
+
+  useEffect(() => {
+    const onChange = (result) => {
+      setScreenInfo(result.screen);
+    };
+
+    Dimensions.addEventListener("change", onChange);
+
+    return () => Dimensions.removeEventListener("change", onChange);
+  }, []);
+
+  return {
+    ...screenInfo,
+    isPortrait: screenInfo.height > screenInfo.width,
+  };
+};
+
+export default useOrientation;
