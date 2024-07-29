08f809d632179bce9577d0c59e025c1c58e79a48
Kalinka5 <dania003d@gmail.com>
2024-07-29 16:35:59 +0300

    Updated Register Page in landscape orientation

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -9,6 +9,10 @@ import Validation from "../validation";
 
 import { LOGIN_PAGE } from "../constants";
 
+import useOrientation from "../useOrientation";
+
+import { Carousel } from "../components/Carousel";
+
 import "../styles/register.css";
 
 function Register() {
@@ -22,6 +26,8 @@ function Register() {
 
   const [errors, setErrors] = useState({});
   const [validFields, setValidFields] = useState({});
+
+  const orientation = useOrientation();
 
   const handleSubmit = async (e) => {
     setLoading(true);
@@ -76,112 +82,231 @@ function Register() {
     <div className="register">
       <div className="login-register">
         {statusMessage}
-        <div className="log-reg-background">
-          <div className="shape reg-shape1"></div>
-          <div className="shape reg-shape2"></div>
-        </div>
-        <form className="log-reg-form reg-p" onSubmit={handleSubmit}>
-          <h2>Registration</h2>
-
-          <div className="reg-input-box">
-            <div className="input-icon">
-              <input
-                className={`${
-                  validFields.usernameIsValid ? "success" : "error"
-                }`}
-                type="text"
-                placeholder="Enter your username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-              <i className="--icon">
-                <IoIosBulb />
-              </i>
-              <span className="tooltiptext name-tip">
-                Username не повинен бути пустим!
-              </span>
+        {orientation.isPortrait ? (
+          <div className="portrait">
+            <div className="log-reg-background">
+              <div className="shape reg-shape1"></div>
+              <div className="shape reg-shape2"></div>
             </div>
-            {errors.username && <p>{errors.username}</p>}
-          </div>
+            <form className="log-reg-form reg-p" onSubmit={handleSubmit}>
+              <h2>Registration</h2>
 
-          <div className="reg-input-box">
-            <div className="input-icon">
-              <input
-                className={`${validFields.emailIsValid ? "success" : "error"}`}
-                type="text"
-                placeholder="user123@gmail.com"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <i className="--icon">
-                <IoIosBulb />
-              </i>
-              <span className="tooltiptext email-tip">
-                Правильний формат адреси електронної пошти складається з 4
-                частин: локальна частина, символ "@", крапка і доменне ім'я.
-              </span>
+              <div className="reg-input-box">
+                <div className="input-icon">
+                  <input
+                    className={`${
+                      validFields.usernameIsValid ? "success" : "error"
+                    }`}
+                    type="text"
+                    placeholder="Enter your username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                  />
+                  <i className="--icon">
+                    <IoIosBulb />
+                  </i>
+                  <span className="tooltiptext name-tip">
+                    Username не повинен бути пустим!
+                  </span>
+                </div>
+                {errors.username && <p>{errors.username}</p>}
+              </div>
+
+              <div className="reg-input-box">
+                <div className="input-icon">
+                  <input
+                    className={`${
+                      validFields.emailIsValid ? "success" : "error"
+                    }`}
+                    type="text"
+                    placeholder="user123@gmail.com"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <i className="--icon">
+                    <IoIosBulb />
+                  </i>
+                  <span className="tooltiptext email-tip">
+                    Правильний формат адреси електронної пошти складається з 4
+                    частин: локальна частина, символ "@", крапка і доменне ім'я.
+                  </span>
+                </div>
+                {errors.email && <p>{errors.email}</p>}
+              </div>
+
+              <div className="reg-input-box">
+                <div className="input-icon">
+                  <input
+                    className={`${
+                      validFields.passwordIsValid ? "success" : "error"
+                    }`}
+                    type="password"
+                    placeholder="Create password"
+                    value={password1}
+                    onChange={(e) => setPassword1(e.target.value)}
+                  />
+                  <i className="--icon">
+                    <IoIosBulb />
+                  </i>
+                  <span className="tooltiptext pass-tip">
+                    Ваш пароль повинен складатися не менше ніж з 8 символів і
+                    містити не менше 1 літери, 1 цифри та 1 спеціального символу
+                    (наприклад, #, @, &)
+                  </span>
+                </div>
+                {errors.password && <p>{errors.password}</p>}
+              </div>
+
+              <div className="reg-input-box">
+                <div className="input-icon">
+                  <input
+                    className={`${
+                      validFields.confPasswordIsValid ? "success" : "error"
+                    }`}
+                    type="password"
+                    placeholder="Confirm password"
+                    value={password2}
+                    onChange={(e) => setPassword2(e.target.value)}
+                  />
+                  <i className="--icon">
+                    <IoIosBulb />
+                  </i>
+                  <span className="tooltiptext confirm-tip">
+                    Підтверджувальний пароль повинен співпадати зі створюваним
+                    паролем!
+                  </span>
+                </div>
+                {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
+              </div>
+
+              <div className="btn-container reg-button">
+                <button type="submit">
+                  Register Now
+                  {loading && <div className="loader"></div>}
+                </button>
+              </div>
+
+              <p>
+                Already have an account?
+                <br />
+                <a href={`${LOGIN_PAGE}`}>Login now</a>
+              </p>
+            </form>
+          </div>
+        ) : (
+          <div className="landscape">
+            <div className="image column">
+              <Carousel />
             </div>
-            {errors.email && <p>{errors.email}</p>}
-          </div>
+            <div className="column">
+              <form className="log-reg-form reg-p" onSubmit={handleSubmit}>
+                <h2>Registration</h2>
 
-          <div className="reg-input-box">
-            <div className="input-icon">
-              <input
-                className={`${
-                  validFields.passwordIsValid ? "success" : "error"
-                }`}
-                type="password"
-                placeholder="Create password"
-                value={password1}
-                onChange={(e) => setPassword1(e.target.value)}
-              />
-              <i className="--icon">
-                <IoIosBulb />
-              </i>
-              <span className="tooltiptext pass-tip">
-                Ваш пароль повинен складатися не менше ніж з 8 символів і
-                містити не менше 1 літери, 1 цифри та 1 спеціального символу
-                (наприклад, #, @, &)
-              </span>
+                <div className="reg-input-box">
+                  <div className="input-icon">
+                    <input
+                      className={`${
+                        validFields.usernameIsValid ? "success" : "error"
+                      }`}
+                      type="text"
+                      placeholder="Enter your username"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <i className="--icon">
+                      <IoIosBulb />
+                    </i>
+                    <span className="tooltiptext name-tip">
+                      Username не повинен бути пустим!
+                    </span>
+                  </div>
+                  {errors.username && <p>{errors.username}</p>}
+                </div>
+
+                <div className="reg-input-box">
+                  <div className="input-icon">
+                    <input
+                      className={`${
+                        validFields.emailIsValid ? "success" : "error"
+                      }`}
+                      type="text"
+                      placeholder="user123@gmail.com"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <i className="--icon">
+                      <IoIosBulb />
+                    </i>
+                    <span className="tooltiptext email-tip">
+                      Правильний формат адреси електронної пошти складається з 4
+                      частин: локальна частина, символ "@", крапка і доменне
+                      ім'я.
+                    </span>
+                  </div>
+                  {errors.email && <p>{errors.email}</p>}
+                </div>
+
+                <div className="reg-input-box">
+                  <div className="input-icon">
+                    <input
+                      className={`${
+                        validFields.passwordIsValid ? "success" : "error"
+                      }`}
+                      type="password"
+                      placeholder="Create password"
+                      value={password1}
+                      onChange={(e) => setPassword1(e.target.value)}
+                    />
+                    <i className="--icon">
+                      <IoIosBulb />
+                    </i>
+                    <span className="tooltiptext pass-tip">
+                      Ваш пароль повинен складатися не менше ніж з 8 символів і
+                      містити не менше 1 літери, 1 цифри та 1 спеціального
+                      символу (наприклад, #, @, &)
+                    </span>
+                  </div>
+                  {errors.password && <p>{errors.password}</p>}
+                </div>
+
+                <div className="reg-input-box">
+                  <div className="input-icon">
+                    <input
+                      className={`${
+                        validFields.confPasswordIsValid ? "success" : "error"
+                      }`}
+                      type="password"
+                      placeholder="Confirm password"
+                      value={password2}
+                      onChange={(e) => setPassword2(e.target.value)}
+                    />
+                    <i className="--icon">
+                      <IoIosBulb />
+                    </i>
+                    <span className="tooltiptext confirm-tip">
+                      Підтверджувальний пароль повинен співпадати зі створюваним
+                      паролем!
+                    </span>
+                  </div>
+                  {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
+                </div>
+
+                <div className="btn-container reg-button">
+                  <button type="submit">
+                    Register Now
+                    {loading && <div className="loader"></div>}
+                  </button>
+                </div>
+
+                <p>
+                  Already have an account?
+                  <br />
+                  <a href={`${LOGIN_PAGE}`}>Login now</a>
+                </p>
+              </form>
             </div>
-            {errors.password && <p>{errors.password}</p>}
           </div>
-
-          <div className="reg-input-box">
-            <div className="input-icon">
-              <input
-                className={`${
-                  validFields.confPasswordIsValid ? "success" : "error"
-                }`}
-                type="password"
-                placeholder="Confirm password"
-                value={password2}
-                onChange={(e) => setPassword2(e.target.value)}
-              />
-              <i className="--icon">
-                <IoIosBulb />
-              </i>
-              <span className="tooltiptext confirm-tip">
-                Підтверджувальний пароль повинен співпадати зі створюваним
-                паролем!
-              </span>
-            </div>
-            {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
-          </div>
-
-          <div className="btn-container reg-button">
-            <button type="submit">
-              Register Now
-              {loading && <div className="loader"></div>}
-            </button>
-          </div>
-
-          <p>
-            Already have an account?
-            <br />
-            <a href={`${LOGIN_PAGE}`}>Login now</a>
-          </p>
-        </form>
+        )}
       </div>
     </div>
   );
