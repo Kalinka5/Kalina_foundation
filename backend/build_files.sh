5dcf74560404b957e147a1e32b5d001855d3b324
Kalinka5 <dania003d@gmail.com>
2024-10-17 13:56:38 +0300

    Edit build_files.sh file

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,2 +1,11 @@
-pip install -r requirements.txt
-python3.12 manage.py collectstatic --noinput
+#!/bin/bash
+
+echo "Building project packages..."
+python3 -m pip install -r requirements.txt
+
+echo "Migrating database..."
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+
+echo "Collecting static files..."
+python3 manage.py collectstatic --noinput
