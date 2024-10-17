83d9a11eff47a4919094e76bd619d51a784ccf2e
Kalinka5 <dania003d@gmail.com>
2024-10-17 15:04:45 +0300

    Add django_filters into INSTALLED_APPS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     'fond_api',
     'rest_framework',
     'rest_framework_simplejwt',
