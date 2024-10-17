1747428546e153b3fc4b3c1e13f52fcfe36df59e
Kalinka5 <dania003d@gmail.com>
2024-10-17 14:55:09 +0300

    Changed env Database names

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -94,7 +94,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env("DB_NAME"),
+        'NAME': env("DB_DATABASE"),
         'USER': env("DB_USER"),
         'PASSWORD': env("DB_PASSWORD"),
         'HOST': env("DB_HOST"),
