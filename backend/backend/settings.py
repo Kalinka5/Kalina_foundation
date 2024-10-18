06ed17dd061c3ccb0c9f3c6f87a57844638622d7
Kalinka5 <dania003d@gmail.com>
2024-10-18 18:05:40 +0300

    Fixing problem with Image url v3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -144,14 +144,12 @@ AWS_S3_REGION_NAME = env("AWS_S3_REGION_NAME")
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
 # For serving static files directly from S3
-AWS_S3_URL_PROTOCOL = 'https'
 AWS_S3_USE_SSL = True
-AWS_S3_VERIFY = True
 
-STATIC_URL = f'{AWS_S3_URL_PROTOCOL}://{AWS_S3_CUSTOM_DOMAIN}/static/'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-MEDIA_URL = f'{AWS_S3_URL_PROTOCOL}://{AWS_S3_CUSTOM_DOMAIN}/media/'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Emailing settings
