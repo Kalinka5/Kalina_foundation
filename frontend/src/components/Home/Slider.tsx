7acde004cb43c54a1fd86f25878355c39fae0f6b
Kalinka5 <dania003d@gmail.com>
2024-12-05 23:04:24 +0200

    Add text into Slider

--- frontend/src/components/Home/Slider.tsx
+++ frontend/src/components/Home/Slider.tsx
@@ -13,6 +13,8 @@ import opticalDevMobile from "../../img/optical_dev_mobile.jpg";
 import transport from "../../img/transport.jpg";
 import transportMobile from "../../img/transport_mobile.jpeg";
 
+import { useTranslation } from "react-i18next";
+
 import { SliderProps } from "../../lib/types";
 
 import "../../styles/home/slider.css";
@@ -23,6 +25,9 @@ function Slider({ page, setPage }: SliderProps) {
   const [alt, setAlt] = useState<string>("");
 
   const isMobile = useMediaQuery("only screen and (max-width: 540px)");
+
+  const { t } = useTranslation();
+
   // Use useMemo to memoize the images array
   const images = useMemo(
     () => [
@@ -77,7 +82,65 @@ function Slider({ page, setPage }: SliderProps) {
 
   return (
     <div className="slider">
-      <img src={image} alt={alt} />
+      <div className="slider-text-image">
+        <div className="text-description">
+          <div className="text-container text-center mg-100">
+            <div className="text-logo-container">
+              <h1 className="logo-text">
+                <span>Kalina</span>&nbsp;
+                <span>Foundataion</span>
+              </h1>
+              <p className="logo-slogan">{t("slider-slogan")}</p>
+            </div>
+          </div>
+          <div className="text-container mg-70 text-columns">
+            <div className="about-container">
+              <h2 className="text-title">{t("slider-header-1")}</h2>
+              <p className="text">{t("slider-text-1")}</p>
+            </div>
+            <div className="about-container right">
+              <h2 className="text-title">{t("slider-header-2")}</h2>
+              <p className="text">{t("slider-text-2")}</p>
+            </div>
+          </div>
+          <div className="text-container mg-150 text-columns">
+            <div className="about-container">
+              <h2 className="text-title">{t("slider-header-3")}</h2>
+              <p className="text list">{t("slider-text-3")}</p>
+              <ul className="list-items bullets">
+                <li>{t("donate-items-1")}</li>
+                <li>{t("donate-items-2")}</li>
+                <li>{t("donate-items-3")}</li>
+                <li>{t("donate-items-4")}</li>
+              </ul>
+            </div>
+            <div className="about-container right">
+              <h2 className="text-title">{t("slider-header-4")}</h2>
+              <p className="text list">{t("slider-text-4")}</p>
+              <div className="container-list">
+                <ul className="list-items bags">
+                  <li>{t("donate-methods-1")}</li>
+                  <li>{t("donate-methods-2")}</li>
+                  <li>{t("donate-methods-3")}</li>
+                  <li>{t("donate-methods-4")}</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="text-container text-center">
+            <h2 className="text-title">{t("slider-header-5")}</h2>
+            <p className="text">
+              {t("slider-text-5")}&nbsp;
+              <span>
+                <a href="/donate" target="_blank">
+                  {t("slider-text-link")}
+                </a>
+              </span>
+            </p>
+          </div>
+        </div>
+        <img className="slider-image" src={image} alt={alt} />
+      </div>
       <button className="prev" onClick={handlePrev}>
         prev
       </button>
--- frontend/src/components/ItemEdit/Titles.tsx
+++ frontend/src/components/ItemEdit/Titles.tsx
@@ -5,9 +5,9 @@ import { TitleProps } from "../../lib/types";
 import "../../styles/itemEdit/titles.css";
 
 export function H1({ children }: TitleProps) {
-  return <h1>{children}</h1>;
+  return <h1 className="item-edit-title">{children}</h1>;
 }
 
 export function H4({ children }: TitleProps) {
-  return <h4>{children}</h4>;
+  return <h4 className="item-edit-title">{children}</h4>;
 }
