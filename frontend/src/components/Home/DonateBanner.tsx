dc46efcc54b32594d8af9d9471222b0409e4bc32
Kalinka5 <dania003d@gmail.com>
2025-06-16 17:58:16 +0300

    Fixed issue with sliding Donate banner

--- frontend/src/components/Home/DonateBanner.tsx
+++ frontend/src/components/Home/DonateBanner.tsx
@@ -4,34 +4,18 @@ import "../../styles/home/donateBanner.css"
 function DonateBanner() {
 	return (
 		<div className="donate-banner">
-			<div className="container">
-				<div className="banner-wrapper">
-					<div className="banner-content">
-						<span>Donate Now</span>
-						<span>•</span>
-						<span>Donate Now</span>
-						<span>•</span>
-						<span>Donate Now</span>
-						<span>•</span>
-						<span>Donate Now</span>
-						<span>•</span>
-						<span>Donate Now</span>
-						<span>•</span>
-						<span>Donate Now</span>
-						<span>•</span>
-						<span>Donate Now</span>
-						<span>•</span>
-						<span>Donate Now</span>
-						<span>•</span>
-						<span>Donate Now</span>
-						<span>•</span>
-						<span>Donate Now</span>
-						<span>•</span>
-						<span>Donate Now</span>
-						<span>•</span>
-						<span>Donate Now</span>
-						<span>•</span>
-					</div>
+			<div className="banner-track">
+				<div className="banner-content">
+					DONATE NOW • DONATE NOW • DONATE NOW • DONATE NOW • DONATE NOW •
+					DONATE NOW • DONATE NOW • DONATE NOW • DONATE NOW • DONATE NOW •
+					DONATE NOW • DONATE NOW • DONATE NOW • DONATE NOW • DONATE NOW •
+					DONATE NOW • DONATE NOW • DONATE NOW • DONATE NOW • DONATE NOW
+				</div>
+				<div className="banner-content">
+					• DONATE NOW • DONATE NOW • DONATE NOW • DONATE NOW • DONATE NOW •
+					DONATE NOW • DONATE NOW • DONATE NOW • DONATE NOW • DONATE NOW •
+					DONATE NOW • DONATE NOW • DONATE NOW • DONATE NOW • DONATE NOW •
+					DONATE NOW • DONATE NOW • DONATE NOW • DONATE NOW • DONATE NOW
 				</div>
 			</div>
 		</div>
